831db106d874de938ab3aeb7b591f443cad80e2a
FlameZZoFF <94693478+FlameZZoFF@users.noreply.github.com>
2022-12-19 22:06:57 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(postgresql://postgres:GmidNTUckiENsJ7Y1z1a@containers-us-west-116.railway.app:7733/railway,{
+module.exports = new Sequelize('postgresql://postgres:GmidNTUckiENsJ7Y1z1a@containers-us-west-116.railway.app:7733/railway',{
   define:{
     timestamps:false
   }
